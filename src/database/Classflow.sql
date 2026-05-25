1c2879778b00cfc17ec7dd36419f98d55fc77895
Sagarnaga-Jesus <233308060610335@cetis61.edu.mx>
2026-05-25 04:13:30 -0600

    bd

--- src/database/Classflow.sql
+++ src/database/Classflow.sql
@@ -1,10 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.4.28-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win64
--- HeidiSQL Versión:             12.15.0.7171
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
@@ -14,12 +7,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
--- Volcando estructura de base de datos para classflow
 CREATE DATABASE IF NOT EXISTS `classflow` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci */;
 USE `classflow`;
 
--- Volcando estructura para tabla classflow.actividades
 CREATE TABLE IF NOT EXISTS `actividades` (
   `id_actividades` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) DEFAULT NULL,
@@ -29,58 +19,92 @@ CREATE TABLE IF NOT EXISTS `actividades` (
   `fecha_entrega` datetime DEFAULT NULL,
   `fecha_agregada` datetime DEFAULT NULL,
   `id_unidad` int(11) DEFAULT NULL,
+  `id_google` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id_actividades`),
   KEY `FK_actividades_unidad` (`id_unidad`),
-  CONSTRAINT `FK_actividades_unidad` FOREIGN KEY (`id_unidad`) REFERENCES `unidad` (`id_unidad`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  CONSTRAINT `FK_actividades_unidad` FOREIGN KEY (`id_unidad`) REFERENCES `unidad` (`id_unidad`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- Volcando datos para la tabla classflow.actividades: ~0 rows (aproximadamente)
 DELETE FROM `actividades`;
+INSERT INTO `actividades` (`id_actividades`, `nombre`, `descripcion`, `tipo`, `valor`, `fecha_entrega`, `fecha_agregada`, `id_unidad`, `id_google`) VALUES
+	(4, 'Prueba 1', 'Prueba de actividad a estudiantes ', 'Actividad', 10, NULL, NULL, 31, '865886988730');
 
--- Volcando estructura para tabla classflow.alumnos
 CREATE TABLE IF NOT EXISTS `alumnos` (
   `id_alumno` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` int(11) DEFAULT NULL,
-  `correo` int(11) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `correo` varchar(200) DEFAULT NULL,
+  `id_google` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id_alumno`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- Volcando datos para la tabla classflow.alumnos: ~0 rows (aproximadamente)
 DELETE FROM `alumnos`;
+INSERT INTO `alumnos` (`id_alumno`, `nombre`, `correo`, `id_google`) VALUES
+	(9, 'Luisito (LuisitoGoat)', NULL, '104240977640611220486'),
+	(10, 'Karla Andres', NULL, '107006843594341589811');
 
--- Volcando estructura para tabla classflow.alumnos_clase
 CREATE TABLE IF NOT EXISTS `alumnos_clase` (
   `id_alumno_clase` int(11) NOT NULL AUTO_INCREMENT,
   `id_alumno` int(11) NOT NULL,
-  `id_clase` int(11) NOT NULL,
+  `id_clase` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`id_alumno_clase`),
-  KEY `FK__clase` (`id_clase`),
   KEY `FK__alumnos` (`id_alumno`),
-  CONSTRAINT `FK__alumnos` FOREIGN KEY (`id_alumno`) REFERENCES `alumnos` (`id_alumno`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK__clase` FOREIGN KEY (`id_clase`) REFERENCES `clase` (`id_clase`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  CONSTRAINT `FK__alumnos` FOREIGN KEY (`id_alumno`) REFERENCES `alumnos` (`id_alumno`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- Volcando datos para la tabla classflow.alumnos_clase: ~0 rows (aproximadamente)
 DELETE FROM `alumnos_clase`;
+INSERT INTO `alumnos_clase` (`id_alumno_clase`, `id_alumno`, `id_clase`) VALUES
+	(8, 9, '865000419090'),
+	(9, 10, '865000419090'),
+	(10, 9, '865000419090'),
+	(11, 10, '865000419090'),
+	(12, 9, '865000419090'),
+	(13, 10, '865000419090'),
+	(14, 9, '865000419090'),
+	(15, 10, '865000419090'),
+	(16, 9, '865000419090'),
+	(17, 10, '865000419090'),
+	(18, 9, '865000419090'),
+	(19, 10, '865000419090'),
+	(20, 9, '865000419090'),
+	(21, 10, '865000419090'),
+	(22, 9, '865000419090'),
+	(23, 10, '865000419090'),
+	(24, 9, '865000419090'),
+	(25, 10, '865000419090'),
+	(26, 9, '865000419090'),
+	(27, 10, '865000419090'),
+	(28, 9, '865000419090'),
+	(29, 10, '865000419090'),
+	(30, 9, '865000419090'),
+	(31, 10, '865000419090'),
+	(32, 9, '865000419090'),
+	(33, 10, '865000419090'),
+	(34, 9, '865000419090'),
+	(35, 10, '865000419090');
 
--- Volcando estructura para tabla classflow.clase
 CREATE TABLE IF NOT EXISTS `clase` (
   `id_clase` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) DEFAULT NULL,
   `descripcion` text DEFAULT NULL,
   `id_profesor` int(11) DEFAULT NULL,
-  `id_google` varchar(50) DEFAULT NULL,
+  `id_google` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id_clase`),
   KEY `id_profesor` (`id_profesor`),
-  CONSTRAINT `FK_clase_profesores` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  CONSTRAINT `FK_clase_profesores` FOREIGN KEY (`id_profesor`) REFERENCES `profesores` (`id_profesor`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- Volcando datos para la tabla classflow.clase: ~0 rows (aproximadamente)
 DELETE FROM `clase`;
 INSERT INTO `clase` (`id_clase`, `nombre`, `descripcion`, `id_profesor`, `id_google`) VALUES
-	(8, 'prueba', '', 6, '865000419090');
+	(1, 'Mate', 'asdfdas', 2, NULL),
+	(2, 'mates', 'holas', 2, NULL),
+	(3, 'mate 1', 'grupo:2', 2, NULL),
+	(4, 'Pruebas', 'Pruebas de unidades', 3, NULL),
+	(5, 'dsdfffs', 'sdffsdf', 2, NULL),
+	(6, 'hola', 'descripcion', 4, NULL),
+	(7, 'prueba', '', 5, '865000419090'),
+	(11, 'Clasr', 'Programación ', 5, '854717875166'),
+	(13, 'Prueba2', '', 5, '854565081343');
 
--- Volcando estructura para tabla classflow.evaluacion
 CREATE TABLE IF NOT EXISTS `evaluacion` (
   `id_evaluacion` int(11) NOT NULL AUTO_INCREMENT,
   `id_alumno` int(11) DEFAULT NULL,
@@ -95,40 +119,47 @@ CREATE TABLE IF NOT EXISTS `evaluacion` (
   CONSTRAINT `FK_evaluacion_alumnos` FOREIGN KEY (`id_alumno`) REFERENCES `alumnos` (`id_alumno`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- Volcando datos para la tabla classflow.evaluacion: ~0 rows (aproximadamente)
 DELETE FROM `evaluacion`;
 
--- Volcando estructura para tabla classflow.profesores
 CREATE TABLE IF NOT EXISTS `profesores` (
   `id_profesor` int(10) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL DEFAULT '0',
   `correo` varchar(100) NOT NULL DEFAULT '0',
-  `id_google` varchar(100) DEFAULT NULL,
-  `foto` text NOT NULL,
+  `foto` text DEFAULT NULL,
+  `fecha_registro` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id_profesor`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- Volcando datos para la tabla classflow.profesores: ~1 rows (aproximadamente)
 DELETE FROM `profesores`;
-INSERT INTO `profesores` (`id_profesor`, `nombre`, `correo`, `id_google`, `foto`) VALUES
-	(6, 'ShadyGalaxy', 'shadysagarnaga@gmail.com', NULL, 'https://lh3.googleusercontent.com/a/ACg8ocI9Z08zsxVfkBayGw5l4fhRCGmDwCVlREUx5DWwPoTNN2WC5Q4=s96-c');
+INSERT INTO `profesores` (`id_profesor`, `nombre`, `correo`, `foto`, `fecha_registro`) VALUES
+	(1, 'Jesus', 'prueba0@gamil.com', NULL, NULL),
+	(2, 'Kenia', 'prueba1@gmail.com', NULL, NULL),
+	(3, 'Pruebas', 'pruebas@gmail.com', NULL, NULL),
+	(4, 'JESUS ANTONIO SAGARNAGA MACIAS', '23308060610335@cetis61.edu.mx', 'https://lh3.googleusercontent.com/a/ACg8ocJU5Y9NoNnuNEz1D_t_LxsiC4tam97XWGj1I0wMXKU8gGuFni0=s96-c', NULL),
+	(5, 'ShadyGalaxy', 'shadysagarnaga@gmail.com', 'https://lh3.googleusercontent.com/a/ACg8ocI9Z08zsxVfkBayGw5l4fhRCGmDwCVlREUx5DWwPoTNN2WC5Q4=s96-c', NULL);
 
--- Volcando estructura para tabla classflow.unidad
 CREATE TABLE IF NOT EXISTS `unidad` (
   `id_unidad` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL DEFAULT '0',
+  `examen` int(11) NOT NULL DEFAULT 0,
+  `proyecto` int(11) NOT NULL DEFAULT 0,
+  `lista` int(11) NOT NULL DEFAULT 0,
+  `actividades` int(11) NOT NULL DEFAULT 0,
+  `extra` int(11) NOT NULL DEFAULT 0,
   `id_clase` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id_unidad`),
   KEY `FK_unidad_clase` (`id_clase`),
-  CONSTRAINT `FK_unidad_clase` FOREIGN KEY (`id_clase`) REFERENCES `clase` (`id_clase`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  CONSTRAINT `FK_unidad_clase` FOREIGN KEY (`id_clase`) REFERENCES `clase` (`id_clase`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
--- Volcando datos para la tabla classflow.unidad: ~0 rows (aproximadamente)
 DELETE FROM `unidad`;
-INSERT INTO `unidad` (`id_unidad`, `nombre`, `id_clase`) VALUES
-	(4, 'Unidad 1', 8),
-	(5, 'unidad 2', 8),
-	(6, 'Unidad 3', 8);
+INSERT INTO `unidad` (`id_unidad`, `nombre`, `examen`, `proyecto`, `lista`, `actividades`, `extra`, `id_clase`) VALUES
+	(1, 'Unidad 1', 0, 0, 0, 0, 0, 1),
+	(2, 'Unidad 2', 0, 0, 0, 0, 0, 1),
+	(3, 'Unidad 4', 0, 0, 0, 0, 0, 2),
+	(31, 'Unidad 1', 20, 20, 20, 20, 20, 7),
+	(32, 'unidad 2', 40, 20, 10, 30, 0, 7),
+	(33, 'unidad 1', 50, 30, 0, 20, 0, 11);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
